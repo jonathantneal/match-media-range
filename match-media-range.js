a38535e67e066eafce26b25f012640a7b97cd051
Arsen Karapetyan <a.karapetyan@corp.mail.ru>
2019-09-10 11:23:09 +0300

    fix: get window in function body

--- match-media-range.js
+++ match-media-range.js
@@ -1,11 +1,9 @@
-// native match media
-const nativeMatchMedia = typeof window !== 'undefined' && window.matchMedia;
 
 // match media polyfilled to support min-max
 export default function matchMedia(query) {
-	if (!nativeMatchMedia) {
-		return null
-	}
+	// native match media
+	const nativeMatchMedia = window.matchMedia;
+	
 	return nativeMatchMedia(
 		String(query).replace(
 			/(?:([-+]?[0-9]*\.?[0-9]+)(%|\w+)\s*(<|>)(=)?(\s*))?(aspect-ratio|color|height|monochrome|resolution|width)(?:\s*(<|>)(=)?(\s*)([-+]?[0-9]*\.?[0-9]+)(%|\w+))?/g,
